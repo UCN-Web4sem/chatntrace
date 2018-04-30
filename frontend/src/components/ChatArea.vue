bc0f9daca83ce19774a286663b76a2cd695e4315
Jensen96 <a.jensen1996@gmail.com>
2018-04-30 11:19:42 +0200

    added chatArea component

---
+++ frontend/src/components/ChatArea.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="chat-area">
+        <ul class="list-group list-group-flush">
+            <li v-for="message in messages" :key="message.id" class="list-group-item">
+                <strong>{{ message.user }}</strong>
+				{{ message.msg }}
+		    </li>
+	    </ul>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default {
+    name: "ChatArea",
+    props: ["messages"],
+	data() {
+		return {};
+	},
+	methods: {
+	}
+};
+</script>
