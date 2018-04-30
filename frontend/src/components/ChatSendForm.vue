43ab2337df280aedacae74b994926c50c8443830
Jensen96 <a.jensen1996@gmail.com>
2018-04-30 11:27:27 +0200

    Created the ChatSendForm component

---
+++ frontend/src/components/ChatSendForm.vue
@@ -0,0 +1,22 @@
+<template>
+   <form @submit="send" action="#">
+       <div class="input-group chat-input">
+		    <input :value="value" v-on:input="$emit('input', $event.target.value)" spellcheck="true" autocomplete="off" class="form-control" aria-label="Message">
+			<button class="btn btn-primary" type="submit">Send</button>
+		</div>
+	</form>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default {
+    name: "ChatArea",
+    props: ["send", "value"],
+	data() {
+		return {};
+	},
+	methods: {
+	}
+};
+</script>
